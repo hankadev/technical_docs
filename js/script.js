e81a5482598447e3fe7a105af048fd0d9ded3fd2
Hana Klingová <hana.klingova@gmail.com>
2018-07-01 17:59:42 +0200

    fix the visibility of menu
    add event listeners for the window widht and adjust the visility of menu
    previously if the menu was hidden on small screen and resize to large, it did not display

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 (function() {
   'use strict';
+  /* listen for the click on the menu icon and show/hide the menu */
   let icon = document.querySelector('#menuIcon');
   let menu = document.querySelector('#navbar ul');
   icon.addEventListener('click', function() {
     menu.style.display == 'block' ?   menu.style.display = 'none' :   menu.style.display = 'block';
+  });
+  /* listen for window width and change the visibility of menu accordingly */
+  window.addEventListener('resize', function () {
+    if (window.innerWidth >= 600) {
+      menu.style.display = 'block';
+    }
+  });
+  window.addEventListener('resize', function () {
+    if (window.innerWidth < 600) {
+      menu.style.display = 'none';
+    }
   });
 }());
